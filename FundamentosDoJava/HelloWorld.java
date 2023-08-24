243040864919dbce7d069736644296e027a4cb05
Booklya <dinheiroebom0103@gmail.com>
2023-08-24 10:30:24 -0300

    Hello world nos fundamentos

---
+++ FundamentosDoJava/HelloWorld.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+
+        /* Print Imprime um texto na saída*/
+        System.out.print("Hello World");
+
+        /* Println da um pulo de linha após o texto de saída */
+        System.out.println("Hello World");
+
+        /* Printf premite formar um texto adicionando váriaveis de todos
+         os tipos a serem escritas na saída */
+        String msg = "Hello World";
+        System.out.printf("%s", msg);
+    }
+}
